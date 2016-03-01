105e901442faff2bf949d972a483b1859211f0ab
harjoe <harjoe@hotmail.com>
2016-03-01 16:39:39 +0800

    1114
    111

---
+++ java/java.helloworld/src/harjo/Car.java
@@ -0,0 +1,19 @@
+package harjo;
+
+public class Car
+{
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
---
+++ java/java.helloworld/src/harjo/test/Tester.java
@@ -0,0 +1,11 @@
+package harjo.test;
+
+import harjo.Car;
+
+public class Tester
+{
+	public static void main(String[] args) throws InterruptedException {
+		
+		Car car = new Car();
+	}
+}
